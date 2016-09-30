280f85ba4cac157034b2fd5fbb0e0e0dc5419ea7
Juraci Vieira <juraci.vieira@gmail.com>
2016-09-30 11:00:49 -0300

    Improving acceptance test

--- tests/acceptance/create-goal-test.js
+++ tests/acceptance/create-goal-test.js
@@ -4,12 +4,15 @@ import moduleForAcceptance from 'pomodorify/tests/helpers/module-for-acceptance'
 moduleForAcceptance('Acceptance | create goal');
 
 test('creating a goal', (assert) => {
+  const description = 'Feel comfortable with Node.js backend development';
+
   visit('/goals');
 
-  fillIn('#input-goal', 'Feel comfortable with Node.js backend development');
+  fillIn('#input-goal', description);
   click('#create');
 
   andThen(() => {
     assert.equal(find('.goal').length, 1);
+    assert.equal(find('.goal:eq(0) p').text(), description);
   });
 });
