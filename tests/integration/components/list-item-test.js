8d1cafbe2e29af25e0fb838bfcf1ea9dd29d130e
Juraci Vieira <juraci.vieira@gmail.com>
2016-09-30 18:06:20 -0300

    Extracting list item to component

---
+++ tests/integration/components/list-item-test.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+let goal;
+
+moduleForComponent('list-item', 'Integration | Component | list item', {
+  integration: true,
+
+  beforeEach() {
+    goal = Ember.Object.create({
+      description: 'Feel comfortable with Node.js development'
+    });
+  }
+});
+
+test('it should render the item description', function(assert) {
+  assert.expect(1);
+
+  this.set('goal', goal);
+  this.render(hbs`{{list-item goal=goal}}`);
+  assert.equal(this.$('p').text().trim(), goal.get('description'));
+});
+
+test('it should send a delete action passing the goal as parameter', function(assert) {
+  assert.expect(1);
+
+  this.set('goal', goal);
+  this.set('routeAction', (item) => {
+    assert.equal(item.description, goal.get('description'));
+  });
+
+  this.render(hbs`{{list-item goal=goal delete=routeAction}}`);
+
+  this.$('.delete').click();
+});
