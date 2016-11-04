8321299e174b51af4aa3db1b869c34203a7cd69c
Juraci Vieira <juraci.vieira@gmail.com>
2016-11-04 16:53:08 -0200

    using closure actions

--- app/components/list-item.js
+++ app/components/list-item.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    delete() {
-      this.sendAction('delete', this.get('goal'));
-    },
-
-    openDialog() {
-      this.sendAction('openDialog', this.get('goal'));
-    }
-  }
 });
--- tests/integration/components/list-item-test.js
+++ tests/integration/components/list-item-test.js
@@ -19,32 +19,8 @@ test('it should render the item description', function(assert) {
   assert.expect(1);
 
   this.set('goal', goal);
-  this.render(hbs`{{list-item goal=goal}}`);
+  this.set('routeAction', () => {});
+
+  this.render(hbs`{{list-item goal=goal openDialog=routeAction delete=routeAction}}`);
   assert.equal(this.$('p').text().trim(), goal.get('description'));
-});
-
-test('it should send a delete action passing the goal as parameter', function(assert) {
-  assert.expect(1);
-
-  this.set('goal', goal);
-  this.set('routeAction', (item) => {
-    assert.equal(item.description, goal.get('description'));
-  });
-
-  this.render(hbs`{{list-item goal=goal delete=routeAction}}`);
-
-  this.$('.delete').click();
-});
-
-test('it should send addTask action passing the current goal as parameter', function(assert) {
-  assert.expect(1);
-
-  this.set('goal', goal);
-  this.set('routeAction', (item) => {
-    assert.equal(item.id, goal.get('id'));
-  });
-
-  this.render(hbs`{{list-item goal=goal openDialog=routeAction}}`);
-
-  this.$('.add-task').click();
 });
