a2256a887d2c53217071a5fd2bcd36c7ba597286
Juraci Vieira <juraci.vieira@gmail.com>
2016-11-04 15:17:16 -0200

    Adding more relevant data for mocking

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,7 +1,29 @@
 export default function(server) {
   server.logging = true;
-  server.create('goal', { description: 'Feel comfortable with Node.js development' });
-  server.create('goal', { description: 'Feel comfortable with front end development' });
+  const goalA = server.create('goal', { description: 'Feel comfortable with Node.js development' });
+  server.create('goal', { description: 'Create a full stack JavaScript web app' });
+  const goalB = server.create('goal', { description: 'Learn hot to create continuous deployment for heroku' });
+  server.create('goal', { description: 'Create a desktop pomodoro app with Ember.js + Electron' });
+
+  server.create('task', {
+    description: 'Codeschool - Real time web with node - lvl 1',
+    goalId: goalA.id
+  });
+
+  server.create('task', {
+    description: 'Codeschool - Building blocks of express.js - lvl 1',
+    goalId: goalA.id
+  });
+
+  server.create('task', {
+    description: 'Spike on Node.js app deploy on heroku',
+    goalId: goalB.id
+  });
+
+  server.create('task', {
+    description: 'Learn how to setup postgres database on heroku',
+    goalId: goalB.id
+  });
 
   /*
     Seed your development database using your factories.
