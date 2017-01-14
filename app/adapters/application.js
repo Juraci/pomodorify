11a94b777bf6e5fc00bfad3fb0e416adfc0bd0fe
Juraci Vieira <juraci.vieira@gmail.com>
2017-01-14 12:03:11 -0200

    adding production host

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,16 @@
 import DS from 'ember-data';
+import Config from 'pomodorify/config/environment';
+
+let host;
+
+if(Config.environment === 'production') {
+  host = 'https://pomodorify-backend.herokuapp.com';
+} else {
+  host = 'http://localhost:3000';
+}
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://localhost:3000',
+  host,
   headers: {
     'Content-Type': 'application/json'
   }
